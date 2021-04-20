a7acfdcaa692b4c373e5f7aac8f6058fd45ae201
Marshal Duncan <marshalduncan@Apples-MacBook-Air.local>
2021-04-20 07:53:39 -0400

    part 2

--- Key.java
+++ Key.java
@@ -3,11 +3,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     private boolean isDown;
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
+       key = keyName;
+       sound = soundFile;
     }
 
     /**
@@ -15,14 +19,14 @@ public class Key extends Actor
      */
     public void act()
     {
-        if ( !isDown && Greenfoot.isKeyDown ("g"))
+        if ( !isDown && Greenfoot.isKeyDown (key))
         {
               setImage ("white-key-down.png");
               play ();
                isDown = true;
         }
         
-        if ( isDown && !Greenfoot.isKeyDown ("g"))
+        if ( isDown && !Greenfoot.isKeyDown (key))
         {
               setImage ("white-key.png");
                isDown = false;
@@ -31,6 +35,6 @@ public class Key extends Actor
     
     public void play()
     {
-        Greenfoot.playSound ("3a.wav");
+        Greenfoot.playSound (sound);
     }
 }
